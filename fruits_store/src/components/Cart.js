f7e8b39fd5e3734eff1b4bab79f14c85db13fef5
giu2000 <giulia.moretti.8712@gmail.com>
2020-03-29 20:19:38 +0200

    removeed comments

--- fruits_store/src/components/Cart.js
+++ fruits_store/src/components/Cart.js
@@ -1,24 +1,5 @@
 import React from 'react';
 
-// const Cart = props => {
-//     return (
-//         <div className='cart'>
-//             <div>Cart</div>
-//             <div>
-//                 {
-//                     props.productsList.map(product =>{
-//                         return (
-//                             <div key={product.id}>{product.id}</div>
-//                         )
-//                     })
-//                 }
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Cart;
-
 export default class Cart extends React.Component{
     componentDidMount(){
         this.props.getCartProducts();
