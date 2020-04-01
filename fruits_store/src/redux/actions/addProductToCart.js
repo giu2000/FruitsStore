2be63ea7e4007d21ef49c20c81b28b3f64df84af
giu2000 <giulia.moretti.8712@gmail.com>
2020-04-01 07:14:38 +0200

    second fix not working

--- fruits_store/src/redux/actions/addProductToCart.js
+++ fruits_store/src/redux/actions/addProductToCart.js
@@ -1,9 +1,14 @@
-import { ADD_PRODUCT_TO_CART_REQUEST, ADD_PRODUCT_TO_CART_SUCCESS, ADD_PRODUCT_TO_CART_ERROR } from "./actionTypes";
+import { 
+    ADD_PRODUCT_TO_CART_REQUEST, 
+    ADD_PRODUCT_TO_CART_SUCCESS, 
+    ADD_PRODUCT_TO_CART_ERROR 
+} from "./actionTypes";
+
 import getCartProducts from './getCartProducts';
 
 const addProductToCartRequest = () => {
     return {
-        type: ADD_PRODUCT_TO_CART_REQUEST
+        type: ADD_PRODUCT_TO_CART_REQUEST,
     }
 }
 
@@ -11,7 +16,7 @@ const addProductToCartSuccess = product => {
     return {
         type: ADD_PRODUCT_TO_CART_SUCCESS,
         payload: {
-            product: product
+            product
         }
     }
 }
@@ -20,31 +25,50 @@ const addProductToCartError = error => {
     return {
         type: ADD_PRODUCT_TO_CART_ERROR,
         payload: {
-            error: error
+            error
         }
     }
 }
 
 const baseUrl = 'http://127.0.0.1:3001/cart';
-const optionsForPOSTRequest = product => {
-    return {
-        method: 'POST',
+
+const getProductIndex = (cart, id) => cart.find(prdct => prdct.id === id);
+
+
+const addProductToCart = product => (dispatch, getState) => {
+    const { cart } = getState();
+    const index = getProductIndex(cart, product.id);
+    let endpoint, method, qty;
+
+    dispatch(addProductToCartRequest());
+
+    if(index >= 1){ //item già nel carrello
+        endpoint = `${baseUrl}/${product.id}`;
+        method = 'PUT';
+        qty = cart.products[index].quantity || 0
+    }
+    else{
+        endpoint = baseUrl;
+        method = 'POST';
+        qty = 0
+    }
+
+    fetch(endpoint, {
+        method,
         headers: {
             'Content-type': 'application/json',
             'Accept': 'application/json'
         },
-        // body: JSON.stringify(product)
-        body: JSON.stringify({...product, quantity: 1})
-    }
-}
-
-const addProductToCart = product => dispatch => {
-    dispatch(addProductToCartRequest());
-    fetch(baseUrl, optionsForPOSTRequest(product))
-        .then(response => response.json())
-        .then(prdct => dispatch(addProductToCartSuccess(prdct)))
-        .catch(error => dispatch(addProductToCartError(error)));
+        body: JSON.stringify({
+            ...product,
+            quantity: qty+1
+        })
+    })
+    .then(response => response.json())
+    .then(prdct => dispatch(addProductToCartSuccess(prdct)))
+    .catch(error => dispatch(addProductToCartError(error)));
     dispatch(getCartProducts());
+
 }
 
 export default addProductToCart;
